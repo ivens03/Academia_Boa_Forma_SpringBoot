aec911c7a5b047886c686c32ee893f82f135263d
ivens magno <ivensmagno@gmail.com>
2025-06-25 01:09:22 -0300

    - busca rapida feito no gestor.html
     01:09 25/06/2025

--- src/main/java/academia/boaForma/gestor/controllers/GestorPrincipal.java
+++ src/main/java/academia/boaForma/gestor/controllers/GestorPrincipal.java
@@ -1,13 +1,27 @@
 package academia.boaForma.gestor.controllers;
 
+import academia.boaForma.alunos.controllers.AlunosController;
+import academia.boaForma.alunos.dtos.DadosListarAlunos;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GestorPrincipal {
+    
+    private final AlunosController alunosController;
+    
+    public GestorPrincipal(AlunosController alunosController) {
+        this.alunosController = alunosController;
+    }
 
     @GetMapping("/homeGestor")
-    public String telaPrincipalGestor() {
+    public String telaPrincipalGestor(@PageableDefault(size = 10) Pageable paginacao, Model model) {
+        Page<DadosListarAlunos> alunos = alunosController.listarAlunos(paginacao);
+        model.addAttribute("alunos", alunos.getContent());
         return "gestor";
     }
 
@@ -30,5 +44,4 @@ public class GestorPrincipal {
     public String telaLogin() {
         return "login";
     }
-
 }
